2ae0d007c5e1bfc5e54ed31c53b6e1613a6bbb74
ix0rai <ix0rai64@gmail.com>
2022-05-13 21:26:57 -0500

    first commit

---
+++ src/main/java/io/ix0rai/tantalisingteas/Tantalisingteas.java
@@ -0,0 +1,10 @@
+package io.ix0rai.tantalisingteas;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tantalisingteas implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("hello fabric world!");
+    }
+}
---
+++ src/main/java/io/ix0rai/tantalisingteas/client/TantalisingteasClient.java
@@ -0,0 +1,13 @@
+package io.ix0rai.tantalisingteas.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TantalisingteasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("hello fabric client!");
+    }
+}
