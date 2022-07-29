333fc5e42e5f096947b628d653a6fc60cc723106
ix0rai <ix0rai64@gmail.com>
2022-07-28 20:44:23 -0500

    apply colour providers to all tea cauldrons

--- src/main/java/io/ix0rai/tantalisingteas/client/TantalisingTeasClient.java
+++ src/main/java/io/ix0rai/tantalisingteas/client/TantalisingTeasClient.java
@@ -1,6 +1,6 @@
 package io.ix0rai.tantalisingteas.client;
 
-import io.ix0rai.tantalisingteas.blocks.StillCauldronBlockEntity;
+import io.ix0rai.tantalisingteas.blocks.BoilingCauldronBlockEntity;
 import io.ix0rai.tantalisingteas.data.NbtUtil;
 import io.ix0rai.tantalisingteas.data.TeaColour;
 import io.ix0rai.tantalisingteas.registry.TantalisingBlocks;
@@ -9,9 +9,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 
 @Environment(EnvType.CLIENT)
 public class TantalisingTeasClient implements ClientModInitializer {
@@ -34,20 +38,23 @@ public class TantalisingTeasClient implements ClientModInitializer {
                 }
         );
 
-        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> {
-            long hex = 0xff000000;
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> getHexFor(view, pos, TantalisingBlocks.STILL_CAULDRON_ENTITY), TantalisingBlocks.STILL_CAULDRON);
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> getHexFor(view, pos, TantalisingBlocks.BOILING_CAULDRON_ENTITY), TantalisingBlocks.BOILING_CAULDRON);
+    }
 
-            if (view != null) {
-                //todo: reliably get the block entity - it's not always present and we need to find a way to fix that
-                var entity = view.getBlockEntity(pos, TantalisingBlocks.STILL_CAULDRON_ENTITY);
-                if (entity.isPresent()) {
-                    StillCauldronBlockEntity blockEntity = entity.get();
-                    String hexString = TeaColour.getFromIngredients(blockEntity.getIngredients()).getHex(NbtUtil.getOverallStrength(blockEntity.getIngredients()));
-                    hex = Long.parseLong(hexString, 16);
-                }
+    private int getHexFor(BlockView view, BlockPos pos, BlockEntityType<?> blockEntityType) {
+        long hex = 0xff000000;
+
+        if (view != null) {
+            var entity = view.getBlockEntity(pos, blockEntityType);
+            if (entity.isPresent()) {
+                var blockEntity = entity.get();
+                NbtList ingredients = ((BoilingCauldronBlockEntity) blockEntity).getIngredients();
+                String hexString = TeaColour.getFromIngredients(ingredients).getHex(NbtUtil.getOverallStrength(ingredients));
+                hex = Long.parseLong(hexString, 16);
             }
+        }
 
-            return (int) hex;
-        }, TantalisingBlocks.STILL_CAULDRON);
+        return (int) hex;
     }
 }
