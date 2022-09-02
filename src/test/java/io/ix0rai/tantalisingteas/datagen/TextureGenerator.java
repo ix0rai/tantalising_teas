4a28dd60e76c1abc6deaf9649f3b816248d2772a
ix0rai <ix0rai64@gmail.com>
2022-09-01 19:30:33 -0500

    texture generator cleanup

--- src/test/java/io/ix0rai/tantalisingteas/datagen/TextureGenerator.java
+++ src/test/java/io/ix0rai/tantalisingteas/datagen/TextureGenerator.java
@@ -16,11 +16,18 @@ public class TextureGenerator {
 
     static void generateTeaOverlays() throws IOException {
         for (TeaColour colour : TeaColour.values()) {
+            // get paths
             String path = AssetGenerator.OVERLAY_GENERATED + "/" + colour.getId();
             String sourcePath = AssetGenerator.OVERLAY_SOURCE + "/" + colour.getId();
 
-            for (int strength = 1; strength <= NbtUtil.MAX_STRENGTH; strength++) {
-                BufferedImage newImage = withAlpha(ImageIO.read(new File(sourcePath + "." + FORMAT)), TeaColour.getAlpha(strength));
+            for (int strength = 1; strength <= NbtUtil.MAX_STRENGTH; strength ++) {
+                // get images
+                BufferedImage sourceImage = ImageIO.read(new File(sourcePath + "." + FORMAT));
+                BufferedImage newImage = new BufferedImage(sourceImage.getWidth(), sourceImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+                // modify alpha
+                int alpha = TeaColour.getAlpha(strength);
+                applyPixelTransforms(sourceImage, newImage, (argb) -> new ARGB(alpha, argb.r, argb.g, argb.b));
 
                 // write the image to a file
                 File file = new File(path + "_s" + strength + "." + FORMAT);
@@ -29,32 +36,46 @@ public class TextureGenerator {
         }
     }
 
-    // todo: alpha
     static void generateCauldronOverlays() throws IOException {
         for (TeaColour colour : TeaColour.values()) {
-            for (int s = 0; s <= NbtUtil.MAX_STRENGTH; s++) {
-                String path = AssetGenerator.CAULDRON_TEXTURES + "/" + colour.getId() + "_tea_cauldron_s" + s;
+            for (int strength = 0; strength <= NbtUtil.MAX_STRENGTH; strength++) {
+                // get paths
+                String path = AssetGenerator.CAULDRON_TEXTURES + "/" + colour.getId() + "_tea_cauldron_s" + strength;
                 String sourcePath = AssetGenerator.CAULDRON_TEXTURES_SOURCE + "/water_texture";
 
+                // get images
                 BufferedImage sourceImage = ImageIO.read(new File(sourcePath + "." + FORMAT));
                 BufferedImage newImage = new BufferedImage(sourceImage.getWidth(), sourceImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
 
-                applyPixelTransforms(sourceImage, newImage, (a, r, g, b) -> {
+                // modify alpha and colour
+                int finalS = strength;
+                applyPixelTransforms(sourceImage, newImage, (argb) -> {
+                    int a = TeaColour.getAlpha(finalS);
+                    int r = argb.r;
+                    int g = argb.g;
+                    int b = argb.b;
+
+                    // checks if the pixel is grayscale
                     if (Objects.equals(r, g) && Objects.equals(g, b)) {
+                        // decrease the colour strength by half of the maximum rgb value
                         int subtract = 255 / 2;
                         r -= subtract;
                         g -= subtract;
                         b -= subtract;
 
+                        // modify the colour according to the passed TeaColour
                         r = MathHelper.clamp(r + colour.getRed(), 0, 255);
                         g = MathHelper.clamp(g + colour.getGreen(), 0, 255);
                         b = MathHelper.clamp(b + colour.getBlue(), 0, 255);
                     }
 
-                    return (a << 24) | (r << 16) | (g << 8) | b;
+                    // return the new colour
+                    return new ARGB(a, r, g, b);
                 });
 
-                writeImage(new File(path + "." + FORMAT), withAlpha(newImage, TeaColour.getAlpha(s)));
+                // write image
+                writeImage(new File(path + "." + FORMAT), newImage);
+                // write mcmeta (identical for all files)
                 try (FileWriter writer = new FileWriter(path + "." + FORMAT + ".mcmeta")) {
                     writer.write("""
                             {
@@ -70,11 +91,7 @@ public class TextureGenerator {
         }
     }
 
-    private interface RgbTransformer<A, R, G, B> {
-        int accept(A a, R r, G g, B b);
-    }
-
-    static void applyPixelTransforms(BufferedImage sourceImage, BufferedImage resultImage, RgbTransformer<Integer, Integer, Integer, Integer> consumer) {
+    static void applyPixelTransforms(BufferedImage sourceImage, BufferedImage resultImage, RgbTransformer transformer) {
         // iterate over every pixel in the image
         for (int x = 0; x < sourceImage.getWidth(); x++) {
             for (int y = 0; y < sourceImage.getHeight(); y++) {
@@ -88,7 +105,8 @@ public class TextureGenerator {
                     int green = (rgb & 0x00ff00) >> 8;
                     int red = (rgb & 0x00ff0000) >> 16;
 
-                    int newRgb = consumer.accept(alpha, red, green, blue);
+                    ARGB newArgb = transformer.transform(new ARGB(alpha, red, green, blue));
+                    int newRgb = (newArgb.a << 24) | (newArgb.r << 16) | (newArgb.g << 8) | newArgb.b;
 
                     // set the rgb of the pixel with the new rgb
                     resultImage.setRGB(x, y, newRgb);
@@ -97,24 +115,20 @@ public class TextureGenerator {
         }
     }
 
+    private interface RgbTransformer {
+        ARGB transform(ARGB argb);
+    }
+
+    private record ARGB(int a, int r, int g, int b) {
+    }
+
     private static void writeImage(File file, BufferedImage image) throws IOException {
         if (!file.exists()) {
-            // noinspection ResultOfMethodCallIgnored
+            // noinspection ResultOfMethodCallIgnored aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             file.getParentFile().mkdirs();
-            // noinspection ResultOfMethodCallIgnored
+            // noinspection ResultOfMethodCallIgnored aaaaaaaaaaaaaaaaaaaaaaaaaaaaaab
             file.createNewFile();
             ImageIO.write(image, FORMAT, file);
         }
     }
-
-    private static BufferedImage withAlpha(BufferedImage image, int alpha) {
-        BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        applyPixelTransforms(image, newImage, (a, r, g, b) -> (alpha << 24) | (r << 16) | (g << 8) | b);
-
-        return newImage;
-    }
-
-    // todo
-    // todo texture info: r = g = b on water overlay grayscale
-    // todo shouldn't be too hard to modify rgb, preserve alpha and write
 }
